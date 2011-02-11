6e17ec3939967534a313cd1d05191a3c18f667cd
diego <diego@gcoop.coop>
2011-02-11 20:18:31 -0300

    cambio init de Partitura

--- partitura.py
+++ partitura.py
@@ -6,8 +6,8 @@ NOTAS = { 'C':1, 'C#':2, 'D':3, 'D#':4, 'E':5,'F':6,
 
 class Partitura:
 
-    def __init__(self):
-        self.notas = []
+    def __init__(self, nombre_archivo):
+        self.leer_desde_csv(nombre_archivo)
 
     def leer_desde_csv(self, nombre_archivo):
         '''
@@ -21,6 +21,5 @@ class Partitura:
 
 
 if __name__ == '__main__':
-    p = Partitura()
-    p.leer_desde_csv('partituras/la_yumba.csv')
+    p = Partitura('partituras/la_yumba.csv')
     print p.notas
