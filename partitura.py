7970e1057f835524eda7fd897e04f4b0d8b07721
diego <diego@gcoop.coop>
2011-02-11 19:05:30 -0300

    Agrego clase Partitura

---
+++ partitura.py
@@ -0,0 +1,23 @@
+import csv
+
+
+class Partitura:
+
+    def __init__(self):
+        self.notas = []
+
+    def leer_desde_csv(self, nombre_archivo):
+        '''
+        Recibe la ruta del archivo CSV donde se encuentra la partitura
+        '''
+        archivo = open(nombre_archivo, 'r')
+        lector_csv = csv.reader(archivo)
+        self.notas = [(int(figura), nota, int(octava)) for
+                      figura, nota, octava in lector_csv]
+
+
+
+if __name__ == '__main__':
+    p = Partitura()
+    p.leer_desde_csv('partituras/la_yumba.csv')
+    print p.notas
