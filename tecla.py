cbd9870a89eb55bd07502af3ff6f1cd0c839223f
Hugo Ruscitti <hugoruscitti@gmail.com>
2011-02-11 19:39:24 -0300

    refactorizando la tecla.

--- tecla.py
+++ tecla.py
@@ -6,11 +6,13 @@ class Tecla(pilas.actores.Actor):
         imagen = "data/" + tipo_de_tecla + ".png"
         pilas.actores.Actor.__init__(self, imagen, x=x, y=y)
         self.definir_centro(("centro", "arriba"))
-        self.sonido = pilas.sonidos.cargar('c.wav')
-        afinacion = 2 ** (nota / 12.0) #para no importar math 
-        self.sonido.definir_pitch(afinacion)
-        if tipo_de_tecla == 'blanca':
-            self.z = 100
+        self._cargar_sonido(nota, pilas)
         
     def pulsar(self):
         self.sonido.reproducir()
+
+    def _cargar_sonido(self, nota, pilas):
+        self.sonido = pilas.sonidos.cargar('c.wav')
+        afinacion = 2 ** (nota / 12.0) #para no importar math
+        self.sonido.definir_pitch(afinacion)
+
