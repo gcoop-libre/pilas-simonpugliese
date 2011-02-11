cf465b8ca350e7e9de2e80f839f9ce1fad5df403
Hugo Ruscitti <hugoruscitti@gmail.com>
2011-02-11 18:45:44 -0300

    Creando un piano nuevo, con teclas sostenidas.

---
+++ piano.py
@@ -0,0 +1,17 @@
+import tecla
+
+class PianoNuevo:
+    def __init__(self, dx, dy):
+        tecla.Tecla('blanca', dx + 0,   dy)
+        tecla.Tecla('blanca', dx + 50,  dy)
+        tecla.Tecla('blanca', dx + 100, dy)
+        tecla.Tecla('blanca', dx + 150, dy)
+        tecla.Tecla('blanca', dx + 200, dy)
+        tecla.Tecla('blanca', dx + 250, dy)
+        tecla.Tecla('blanca', dx + 300, dy)
+        
+        tecla.Tecla('negra', dx + 25, dy)
+        tecla.Tecla('negra', dx + 75, dy)
+        
+        tecla.Tecla('negra', dx + 225, dy)
+        tecla.Tecla('negra', dx + 275, dy)
