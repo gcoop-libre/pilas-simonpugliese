e1cde5cb2af898c9b470da6fc9ea33de6deccdea
diego <diego@gcoop.coop>
2011-02-11 19:19:46 -0300

    Modifico la representación interna de las notas

--- partitura.py
+++ partitura.py
@@ -1,6 +1,9 @@
 import csv
 
 
+NOTAS = { 'C':1, 'C#':2, 'D':3, 'D#':4, 'E':5,'F':6,
+          'F#':7, 'G':8, 'G#':9, 'A':10, 'A#':11, 'B':12 }
+
 class Partitura:
 
     def __init__(self):
@@ -12,7 +15,7 @@ class Partitura:
         '''
         archivo = open(nombre_archivo, 'r')
         lector_csv = csv.reader(archivo)
-        self.notas = [(int(figura), nota, int(octava)) for
+        self.notas = [(int(figura), NOTAS[nota], int(octava)) for
                       figura, nota, octava in lector_csv]
 
 
