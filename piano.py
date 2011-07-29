630a52156445a1e6de0ce39a7f74d43fe0d4d8d2
ariel <ariel@gcoop.coop>
2011-07-29 19:25:34 -0300

    modifico la representacion de las notas
    modifico la representacion de las notas para usar un archivo
    independiente para cada tecla

--- piano.py
+++ piano.py
@@ -13,18 +13,20 @@ class PianoNuevo:
     def _crear_teclas(self, dx, dy):
         ancho = 37
         contador = 0
-        negras = [1, 3, 6, 8, 10]
+        octavas = 2
 
-        for nota in xrange(12, 36):
-            if nota%12 in negras:
-                color = 'negra'
-                posicion_x = dx + (ancho * (contador) - ancho/2)
-            else:
-                color = 'blanca'
-                posicion_x = dx + (ancho * contador)
-                contador += 1
-            
-            self.teclas[nota] = tecla.Tecla(color, nota, posicion_x, dy)
+        for octava in range(1, octavas + 1):
+            for nota in ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']:
+                if nota.find("#") != -1:
+                    color = 'negra'
+                    posicion_x = dx + (ancho * (contador) - ancho/2)
+                else:
+                    color = 'blanca'
+                    posicion_x = dx + (ancho * contador)
+                    contador += 1
+
+              nombre_nota = "%s%d" % (nota, octava)
+              self.teclas[nombre_nota] = tecla.Tecla(color, nombre_nota, posicion_x, dy)
 
     def cuando_hace_click(self, evento):
         tecla = pilas.actores.utils.obtener_actor_en(evento.x, evento.y)
@@ -37,23 +39,23 @@ class PianoNuevo:
 
     def presiona_nota_teclado(self, evento):
         mapa_teclas = { 
-            u'z':12,
-            u's':13,
-            u'x':14,
-            u'd':15,
-            u'c':16,
-            u'v':17,
-            u'g':18,
-            u'b':19,
-            u'h':20,
-            u'n':21,
-            u'j':22,
-            u'm':23,
-            u',':24,
-            u'l':25,
-            u'.':26,
-            u'ñ':27,
-            u'-':28,
+            u'z':'C1',
+            u's':'C#1',
+            u'x':'D1',
+            u'd':'D#1',
+            u'c':'E1',
+            u'v':'F1',
+            u'g':'F#1',
+            u'b':'G1',
+            u'h':'G#1',
+            u'n':'A1',
+            u'j':'A#1',
+            u'm':'B1',
+            u',':'C2',
+            u'l':'C#2',
+            u'.':'D2',
+            u'ñ':'D#2',
+            u'-':'E2',
             }
 
         numero_tecla = mapa_teclas.get(evento.codigo, None)
--- tecla.py
+++ tecla.py
@@ -5,9 +5,9 @@ class Tecla(pilas.actores.Actor):
     def __init__(self, tipo_de_tecla, nota, x, y):
         imagen = "data/" + tipo_de_tecla + ".png"
         self.nota = nota
+        self.sonido = pilas.sonidos.cargar(nota + '.wav')
         pilas.actores.Actor.__init__(self, imagen, x=x, y=y)
         self.definir_centro(("centro", "arriba"))
-        self._cargar_sonido(nota, pilas)
         if tipo_de_tecla == 'blanca':
             self.z = 100
         
@@ -18,12 +18,3 @@ class Tecla(pilas.actores.Actor):
             pass
         self.escala = 0.95
         self.escala = [1], 0.5
-
-    def _cargar_sonido(self, numero_nota, pilas):
-        notas = {
-            3: 'c.wav',
-        }
-        try:
-            self.sonido = pilas.sonidos.cargar(notas[numero_nota])
-        except:
-            pass
