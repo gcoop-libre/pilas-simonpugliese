dd80f280354923c30dde846519ffa2e8446f8ae4
Hugo Ruscitti <hugoruscitti@mojito.(none)>
2011-07-29 17:28:38 -0300

    moviendo el piano mas abajo en la pantalla.

--- ejecutar.py
+++ ejecutar.py
@@ -9,9 +9,8 @@ from pilas.actores import Animado
 pilas.iniciar(usar_motor='qt', titulo='Simon pugliese')
 
 
-pilas.avisar('Usa alt+q para salir.')
 pilas.fondos.Pasto()
-b = piano.PianoNuevo(-300, 200)
+b = piano.PianoNuevo(-240, -75)
 p = partitura.Partitura('partituras/la_yumba.csv')
 interprete = Interprete(p, b)
 
